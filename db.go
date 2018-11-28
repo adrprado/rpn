2eb1f9150d7602dc94182829fb8d6a78bde307d9
Adriano Prado <prado@gmx.net>
2018-11-28 18:17:02 -0200

    Change db name

--- db.go
+++ db.go
@@ -10,7 +10,7 @@ import (
 // openDatabase to be used by parsers and reporting
 //
 func openDatabase() (db *sql.DB, err error) {
-	db, err = sql.Open("sqlite3", dataDir+"/cvm.db")
+	db, err = sql.Open("sqlite3", dataDir+"/rapina.db")
 	if err != nil {
 		return db, errors.Wrap(err, "database open failed")
 	}
